6d27f74393846b93002f48686d111073e9fac2bf
Anaël iMac <anael@barodine.fr>
2022-07-19 06:48:31 +0200

    fix: suppression du lien vers le plan du site et refactoring du PHP dans le HTML

--- cadre.php
+++ cadre.php
@@ -203,7 +203,7 @@
                 <p>Tribu-Terre est une association étudiante apolitique à but non lucrative régie par la loi du 1er juillet 1901.</p>
                 <p><a href="/plan-du-site/">Plan du site</a> | <a href="/mentions-legales/">Mentions légales</a></p>
                 <p>Tribu-Terre 2021 | 1A Rue de la Férollerie, 45071, Orléans Cedex 2</p>
-                <p><strong>Site Tribu-Terre version <?php echo VERSION_SITE ?></strong></p>
+                <p><strong>Site Tribu-Terre version <?= VERSION_SITE ?></strong></p>
                 <p><small>Développé avec ❤️ par Anaël BARODINE</small></p>
             </div>
         </div>
