a4a8d996b02cbb8d8b856c9e0aa1ac91ee797430
Anaël iMac <anael@barodine.fr>
2022-08-02 20:36:47 +0200

    feat: Affichage des miniature des journaux.

--- journaux/gabarit.php
+++ journaux/gabarit.php
@@ -5,12 +5,21 @@
         <?php if ($journal->count % 3 == 0): ?><div class="row"><?php endif ?>
             <div class="col-sm-4">
                 <div class="well">
-                    <h3><?= $journal->titre ?></h3>
-                    <time datetime="<?= $journal->date ?>"><?= preg_replace('/^[^ ]* /', '', genererDate($journal->date)) ?></time>
-                    <a href="<?= $journal->pdf ?>" class="btn btn-var btn-block">
-                        <h4 class="alterneur-grand-tres-petit"><img src="/resources/imgPdf.svg" height="28" alt="(PDF)">&emsp;Lire en ligne</h4>
-                        <h4 class="alterneur-petit">Lire</h4>
-                    </a>
+                    <div class="row">
+                        <h3><?= $journal->titre ?></h3>
+                    </div>
+                    <div class="row">
+                        <time datetime="<?= $journal->date ?>"><?= preg_replace('/^[^ ]* /', '', genererDate($journal->date)) ?></time>
+                    </div>
+                    <div class="row">
+                        <img class="miniature" src="<?= "thumbnails/$journal->pdf.webp" ?>" alt="miniatureJournal">
+                    </div>
+                    <div class="row">
+                        <a href="<?= $journal->pdf ?>" class="btn btn-var btn-block" style="position: relative; z-index: 1;">
+                            <h4 class="alterneur-grand-tres-petit"><img src="/resources/svg/imgPdf.svg" height="28" alt="(PDF)">&emsp;Lire en ligne</h4>
+                            <h4 class="alterneur-petit">Lire</h4>
+                        </a>
+                    </div>
                 </div>
             </div>
         <?php if ($journal->count == count($journaux) - 1 || ($journal->count + 1) % 3 == 0): ?></div><?php endif ?>
