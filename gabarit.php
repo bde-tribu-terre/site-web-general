9c2dc55b187b9990a86fe90852377d0e19f3bfc1
Anaël iMac <anael@barodine.fr>
2022-08-03 23:27:08 +0200

    feat: Changement page d'accueil

--- gabarit.php
+++ gabarit.php
@@ -1,7 +1,9 @@
-<div class="container">
+<div class="container text-center">
+    <h1>Tribu-Terre</h1>
+    <hr>
     <div class="row">
-        <div class="col-sm-8">
-            <h3 class="text-center">L'équipe actuelle !</h3>
+        <div class="col-sm-12">
+            <h2>L'équipe actuelle !</h2>
             <a href="/association">
                 <img
                     class="arrondi ombre"
@@ -10,19 +12,6 @@
                     style="width: 100%; min-height: 200px;"
                 >
             </a>
-        </div>
-        <div class="col-sm-4">
-            <h3 class="text-center">Les derniers journaux !</h3>
-            <?php foreach ($journaux as $journal): ?>
-            <div class="well text-center">
-                <h4><?= $journal->titre ?></h4>
-                <time datetime="<?= $journal->date ?>"><?= preg_replace('/^[^ ]* /', '', genererDate($journal->date)) ?></time>
-                <a href="/journaux/<?= $journal->pdf ?>" class="btn btn-var btn-block">
-                    <span class="alterneur-grand-tres-petit"><img src="/resources/svg/imgPdf.svg" height="28" alt="(PDF)">&emsp;Lire en ligne</span>
-                    <span class="alterneur-petit">Lire</span>
-                </a>
-            </div>
-            <?php endforeach; ?>
         </div>
     </div>
 </div>
