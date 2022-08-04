462ad92f8b6cd9080797a725524d9f132e937d7e
Anaël iMac <anael@barodine.fr>
2022-08-04 03:41:55 +0200

    feat: Modification pour pouvoir faire la requête de plusieurs fichiers à la fois

--- autoloaded/App/Checker/ThumbnailChecker.class.php
+++ autoloaded/App/Checker/ThumbnailChecker.class.php
@@ -21,30 +21,52 @@ class ThumbnailChecker {
 
     // Méthodes.
     public function check(): bool {
+        // Variable qui va savoir si quelque chose a été modifié ou non.
         $modif = false;
 
+        // Nom du dossier recueillant les miniatures.
         $thumbnailDirectory = $this->pathToLocation . "thumbnails/";
 
+        // Si ledit dossier n'existe pas, le créer.
         if (!file_exists($thumbnailDirectory) || is_dir($thumbnailDirectory)) {
             mkdir($thumbnailDirectory);
         }
 
+        // Tableau contenant les miniatures à générer.
+        $missThnPdfFiles = array();
+
+        // Pour chaque fichier dans le dossier des PDF...
         foreach (scandir($this->pathToLocation) as $file) {
-            $thumbnailFileName = $thumbnailDirectory . $file . ".webp";
+            // Si le fichier se termine par .pdf et que le fichier de miniature n'existe pas déjà.
+            if (str_ends_with($file, ".pdf") && !file_exists($thumbnailDirectory . $file . ".webp")) {
 
-            if (str_ends_with($file, ".pdf") && !file_exists($thumbnailFileName)) {
+                // Ajout des fichiers dans le tableau.
+                $missThnPdfFiles[] = $file;
 
-                $transloaditResult = TransloaditRequest::new([$this->pathToLocation . $file])->execute(TransloaditRequest::PDF_THUMBNAIL);
+            }
+        }
 
-                $success = file_put_contents($thumbnailFileName, file_get_contents($transloaditResult[0]));
+        if (!empty($missThnPdfFiles)) {
+            // Execution de la requête sur l'API Transloadit.
+            $transloaditResults = TransloaditRequest::new(array_map(fn($file): string => $this->pathToLocation . $file, $missThnPdfFiles))->execute(TransloaditRequest::PDF_THUMBNAIL);
 
+            // Pour chaque résultat de requête manquante...
+            foreach ($transloaditResults as $index => $transloaditResult) {
+
+                $success = file_put_contents(
+                    $thumbnailDirectory . $missThnPdfFiles[$index] . ".webp",
+                    file_get_contents($transloaditResult)
+                );
+
+                // Log.
                 if ($success) {
-                    Log::log("[thumbnail] Thumbnail image of PDF file at URI 'https://" . $_SERVER["HTTP_HOST"] . "$this->pathToLocation$file' not found: created thumbnail on Transloadit and successfully downloaded at '$thumbnailFileName'");
-                } else {
-                    Log::log("[thumbnail] Thumbnail image of PDF file at URI 'https://" . $_SERVER["HTTP_HOST"] . "$this->pathToLocation$file' not found: created thumbnail on Transloadit but unsuccessfully downloaded at '$thumbnailFileName'");
-                }
+                    Log::log("[thumbnail] Thumbnail image of PDF file at URI 'https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . "$this->pathToLocation$missThnPdfFiles[$index]' not found at URI 'https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . "$thumbnailDirectory$missThnPdfFiles[$index].webp': created thumbnail on Transloadit and successfully downloaded");
 
-                $modif = true;
+                    // Quelque chose a été modifié.
+                    $modif = true;
+                } else {
+                    Log::log("[thumbnail] Thumbnail image of PDF file at URI 'https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . "$this->pathToLocation$missThnPdfFiles[$index]' not found at URI 'https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . "$thumbnailDirectory$missThnPdfFiles[$index].webp': created thumbnail on Transloadit but unsuccessfully downloaded");
+                }
             }
         }
 
