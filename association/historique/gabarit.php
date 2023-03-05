43fea130d4ac2b4f1d65d8bc2a1c21875bf02dd9
Anaël iMac <anael@barodine.fr>
2023-03-05 17:13:08 +0100

    feat: Ajout de davantage de mandats.

--- association/historique/gabarit.php
+++ association/historique/gabarit.php
@@ -13,14 +13,34 @@
         <h3><i>Présidence de Jiliane <span class="pc">Bourriot</span></i></h3>
         <div class="histo-compo">
             <img class="histo-compo-bg" src="/resources/png/fondMandatViolette.png" alt="Fond mandat violette">
-            <img class="histo-compo-fg" src="/resources/jpg/mandatViolette.jpg" alt="Photo mandat violette">
+            <img class="histo-compo-fg" style="top: 18%;" src="/resources/jpg/mandatViolette.jpg" alt="Photo mandat violette">
         </div>
         <hr>
         <h2>2021 - 2022 | Mandat Artichaut</h2>
         <h3><i>Présidence d'Anaël <span class="pc">Barodine</span></i></h3>
         <div class="histo-compo">
             <img class="histo-compo-bg" src="/resources/png/fondMandatArtichaut.png" alt="Fond mandat artichaut">
-            <img class="histo-compo-fg" src="/resources/jpg/mandatArtichaut.jpg" alt="Photo mandat artichaut">
+            <img class="histo-compo-fg" style="top: 18%;" src="/resources/jpg/mandatArtichaut.jpg" alt="Photo mandat artichaut">
+        </div>
+        <hr>
+        <h2>2020 - 2021</h2>
+        <h3><i>Présidence de Simon <span class="pc">Medelli</span></i></h3>
+        <div class="histo-logo">
+            <img src="/resources/png/imgLogoMini.png" alt="Logo de Tribu-Terre">
+        </div>
+        <hr>
+        <h2>2019 - 2020</h2>
+        <h3><i>Présidence de Riad <span class="pc">Benchekra</span></i></h3>
+        <div class="histo-compo">
+            <img class="histo-compo-bg" src="/resources/png/fondMandatAncien.png" alt="Fond mandat 2019-2020">
+            <img class="histo-compo-fg" style="top: 18%;" src="/resources/jpg/mandat20192020.jpg" alt="Photo mandat 2019-2020">
+        </div>
+        <hr>
+        <h2>2018 - 2019</h2>
+        <h3><i>Présidence de Quentin <span class="pc">Baju</span></i></h3>
+        <div class="histo-compo">
+            <img class="histo-compo-bg" src="/resources/png/fondMandatAncien.png" alt="Fond mandat 2018-2019">
+            <img class="histo-compo-fg" style="top: 18%;" src="/resources/jpg/mandat20182019.jpg" alt="Photo mandat 2018-2019">
         </div>
     </div>
     <div class="col-sm-2"></div>
