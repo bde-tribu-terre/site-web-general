4c5b5cf9920865e0be08733fba2469b3cdcf03e1
Anaël <anael@barodine.fr>
2023-04-28 18:59:32 +0200

    fix: Regénération de la sitemap index en cas d'existence mais d'illisibilité.

--- autoloaded/App/Checker/SitemapChecker.class.php
+++ autoloaded/App/Checker/SitemapChecker.class.php
@@ -4,6 +4,7 @@ namespace App\Checker;
 
 use App\Log;
 use App\Request\SqlRequest;
+use Throwable;
 
 class SitemapChecker {
     // Attributs statiques.
@@ -62,40 +63,49 @@ class SitemapChecker {
     public function check(): bool {
         $modified = false;
 
-        // Vérification de l'existence de la sitemapindex.
-        $sitemapIndexExists = file_exists($this->root . "sitemapindex.xml");
-
+        // Seulement si le fichier existe alors, on peut tenter de le lire.
         if (file_exists($this->root . "sitemapindex.xml")) {
-            // Chargement de la sitemapindex.
-            $sitemapIndexXmlElement = simplexml_load_file($this->root . "sitemapindex.xml");
+            try {
+                // Chargement de la sitemapindex.
+                $sitemapIndexXmlElement = simplexml_load_file($this->root . "sitemapindex.xml");
 
-            // Vérification que toutes les sitemaps dynamiques sont bien présentes.
-            // Listage des sitemaps dynamiques pas indexées.
-            $notIndexedSitemapsIndexes = array();
-            for ($i = 0; $i < count(SitemapChecker::DYNAMIC_SITEMAPS); $i++) $notIndexedSitemapsIndexes[] = $i;
-            foreach ($sitemapIndexXmlElement as $sitemapIndexSitemapXmlElement) {
-                // Pour chaque sitemap on parcourt la liste des sitemaps dynamiques à MÀJ pour voir s'il y a correspondance.
-                foreach (SitemapChecker::DYNAMIC_SITEMAPS as $i=>$dynamicSitemap) {
-                    // Vérification qu'il s'agit d'une correspondance en utilisant la localisation du fichier.
-                    if ($sitemapIndexSitemapXmlElement->loc == "https://" . $_SERVER["HTTP_HOST"] . $dynamicSitemap[0]) {
-                        unset($notIndexedSitemapsIndexes[$i]);
+                // Vérification que toutes les sitemaps dynamiques sont bien présentes.
+                // Listage des sitemaps dynamiques pas indexées.
+                $notIndexedSitemapsIndexes = array();
+                for ($i = 0; $i < count(SitemapChecker::DYNAMIC_SITEMAPS); $i++) $notIndexedSitemapsIndexes[] = $i;
+                foreach ($sitemapIndexXmlElement as $sitemapIndexSitemapXmlElement) {
+                    // Pour chaque sitemap on parcourt la liste des sitemaps dynamiques à MÀJ pour voir s'il y a correspondance.
+                    foreach (SitemapChecker::DYNAMIC_SITEMAPS as $i => $dynamicSitemap) {
+                        // Vérification qu'il s'agit d'une correspondance en utilisant la localisation du fichier.
+                        if ($sitemapIndexSitemapXmlElement->loc == "https://" . $_SERVER["HTTP_HOST"] . $dynamicSitemap[0]) {
+                            unset($notIndexedSitemapsIndexes[$i]);
+                        }
                     }
                 }
-            }
 
-            // Ajout des sitemaps dynamiques pas indexées.
-            foreach ($notIndexedSitemapsIndexes as $i) {
-                $newURI = $sitemapIndexXmlElement->addChild("sitemap");
-                $newURI->addChild("loc", "https://" . $_SERVER["HTTP_HOST"] . SitemapChecker::DYNAMIC_SITEMAPS[$i][0]);
-                $newURI->addChild("lastmod", date("Y-m-d\TH:i:sP", 0));
-            }
+                // Ajout des sitemaps dynamiques pas indexées.
+                foreach ($notIndexedSitemapsIndexes as $i) {
+                    $newURI = $sitemapIndexXmlElement->addChild("sitemap");
+                    $newURI->addChild("loc", "https://" . $_SERVER["HTTP_HOST"] . SitemapChecker::DYNAMIC_SITEMAPS[$i][0]);
+                    $newURI->addChild("lastmod", date("Y-m-d\TH:i:sP", 0));
+                }
 
-            // Log
-            if (count($notIndexedSitemapsIndexes) != 0) {
-                $modified = true;
-                Log::log("[sitemap] Sitemaps index at URI 'https://" . $_SERVER["HTTP_HOST"] . "/sitemapindex.xml' was incomplete: added " . count($notIndexedSitemapsIndexes) . " new sitemaps URI; new sitemaps index contains " . count(SitemapChecker::DYNAMIC_SITEMAPS) . " sitemap URLs");
+                // Log
+                if (count($notIndexedSitemapsIndexes) != 0) {
+                    $modified = true;
+                    Log::log("[sitemap] Sitemaps index at URI 'https://" . $_SERVER["HTTP_HOST"] . "/sitemapindex.xml' was incomplete: added " . count($notIndexedSitemapsIndexes) . " new sitemaps URI; new sitemaps index contains " . count(SitemapChecker::DYNAMIC_SITEMAPS) . " sitemap URLs");
+                }
+            } catch (Throwable $e) {
+                // Erreur de lecture du fichier, donc on le supprime pour le regénérer.
+                unlink($this->root . "sitemapindex.xml");
+
+                // Log
+                Log::log("[sitemap] Sitemaps index at URI 'https://" . $_SERVER["HTTP_HOST"] . "/sitemapindex.xml' exists and is incomplete, but an exception occurred trying to read the file: " . $e->getMessage() . "; deleting the file in order to validly regenerate");
             }
-        } elseif (!$sitemapIndexExists) {
+        }
+
+        // Si on n'est pas parvenu à valider le fichier sitemap.
+        if (!file_exists($this->root . "sitemapindex.xml")) {
             // Regénération de la sitemapindex à partir des données des sitemaps dynamiques.
             $sitemapIndexXmlElement = simplexml_load_string(<<<EOF
 <?xml version="1.0" encoding="UTF-8"?>
