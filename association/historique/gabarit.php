d2bc3c89596e1a07c6b15505f4d9b6448e1ce9f9
Anaël <anael@barodine.fr>
2023-11-21 12:16:57 +0100

    feat: Changement des images de fond des mandats et lueur pour du vectoriel.

--- association/historique/gabarit.php
+++ association/historique/gabarit.php
@@ -47,17 +47,17 @@
         <h2>2022 - 2023 | Mandat Violette</h2>
         <h3><i>Présidence de Jiliane <span class="pc">Bourriot</span></i></h3>
         <div class="histo-compo">
-            <img class="histo-compo-bg" src="/resources/png/fondMandatViolette.png" alt="Fond mandat violette">
+            <img class="histo-compo-bg" src="/resources/svg/fondMandatViolette.svg" alt="Fond mandat violette">
             <img class="histo-compo-fg" style="top: 18%;" src="/resources/jpg/mandatViolette.jpg" alt="Photo mandat violette">
-            <img class="histo-compo-lu" style="top: 18%;" src="/resources/png/lueurMandat.png" alt="Lueur">
+            <img class="histo-compo-lu" style="top: 18%;" src="/resources/svg/lueurMandat.svg" alt="Lueur">
         </div>
         <hr>
         <h2>2021 - 2022 | Mandat Artichaut</h2>
         <h3><i>Présidence d'Anaël <span class="pc">Barodine</span></i></h3>
         <div class="histo-compo">
-            <img class="histo-compo-bg" src="/resources/png/fondMandatArtichaut.png" alt="Fond mandat artichaut">
+            <img class="histo-compo-bg" src="/resources/svg/fondMandatArtichaut.svg" alt="Fond mandat artichaut">
             <img class="histo-compo-fg" style="top: 18%;" src="/resources/jpg/mandatArtichaut.jpg" alt="Photo mandat artichaut">
-            <img class="histo-compo-lu" style="top: 18%;" src="/resources/png/lueurMandat.png" alt="Lueur">
+            <img class="histo-compo-lu" style="top: 18%;" src="/resources/svg/lueurMandat.svg" alt="Lueur">
         </div>
         <hr>
         <h2>2020 - 2021</h2>
@@ -65,7 +65,7 @@
         <div class="histo-compo">
             <img class="histo-compo-bg" src="/resources/png/fondMandatAncien.png" alt="Fond mandat 2020-2021">
             <img class="histo-compo-fg" style="top: 18%;" src="/resources/jpg/mandat20202021.jpg" alt="Photo mandat 2020-2021">
-            <img class="histo-compo-lu" style="top: 18%;" src="/resources/png/lueurMandat.png" alt="Lueur">
+            <img class="histo-compo-lu" style="top: 18%;" src="/resources/svg/lueurMandat.svg" alt="Lueur">
         </div>
         <hr>
         <h2>2019 - 2020</h2>
@@ -73,7 +73,7 @@
         <div class="histo-compo">
             <img class="histo-compo-bg" src="/resources/png/fondMandatAncien.png" alt="Fond mandat 2019-2020">
             <img class="histo-compo-fg" style="top: 18%;" src="/resources/jpg/mandat20192020.jpg" alt="Photo mandat 2019-2020">
-            <img class="histo-compo-lu" style="top: 18%;" src="/resources/png/lueurMandat.png" alt="Lueur">
+            <img class="histo-compo-lu" style="top: 18%;" src="/resources/svg/lueurMandat.svg" alt="Lueur">
         </div>
         <hr>
         <h2>2018 - 2019</h2>
@@ -81,7 +81,7 @@
         <div class="histo-compo">
             <img class="histo-compo-bg" src="/resources/png/fondMandatAncien.png" alt="Fond mandat 2018-2019">
             <img class="histo-compo-fg" style="top: 18%;" src="/resources/jpg/mandat20182019.jpg" alt="Photo mandat 2018-2019">
-            <img class="histo-compo-lu" style="top: 18%;" src="/resources/png/lueurMandat.png" alt="Lueur">
+            <img class="histo-compo-lu" style="top: 18%;" src="/resources/svg/lueurMandat.svg" alt="Lueur">
         </div>
     </div>
     <div class="col-sm-2"></div>
